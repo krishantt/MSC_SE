f1a6e555c57f1ab6479293b5c83e71ee8b52cfa5
Baebabe <baibhavs45@gmail.com>
2024-07-09 19:59:09 +0545

    .

--- Dockerfile
+++ Dockerfile
@@ -16,7 +16,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . /app/
 
 # Expose port 8000 for the Django app
-EXPOSE 8000
+EXPOSE 7188
 
 # Run the application
-CMD ["python", "manage.py", "runserver",'0.0.0.0:8000']
+CMD ["python", "manage.py", "runserver",'0.0.0.0:7188']
--- manage.py
+++ manage.py
@@ -12,5 +12,12 @@ if __name__ == "__main__":
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
+    if len(sys.argv) == 1:
+        sys.argv.append('runserver')
+        sys.argv.append('127.0.0.1:7188')
+    elif len(sys.argv) == 2 and sys.argv[1] == 'runserver':
+        sys.argv.append('127.0.0.1:7188')
+
     execute_from_command_line(sys.argv)
     
